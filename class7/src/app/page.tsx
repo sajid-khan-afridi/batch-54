22a6db71324f3c827284059b79f40a5e52f503ad
Sajid Khan Afridi <uetianafridi@gmail.com>
2024-05-05 10:48:19 +0500

    class7

---
+++ class7/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="grid grid-cols-2 h-screen">
+      <div className="bg-green-400 flex justify-center items-center">1</div>
+      <div className="bg-red-400">2</div>
+      <div className="bg-slate-400">3</div>
+      <div className="bg-gray-800">4</div>
+    </div>
+  );
+};
+
+export default page;
